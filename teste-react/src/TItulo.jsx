fd102b3719e72b934e5d1815724fa2023e87f720
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-08-22 12:22:37 -0300

    partindo para rotas

--- teste-react/src/TItulo.jsx
+++ teste-react/src/TItulo.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
 function Titulo({cor}){
     const [texto, setTexto] = useState("Título Padrão")
+    const [inputText, setInputText] = useState("")
 
+    function mudarTexto(){
+        setTexto(inputText)
+    }
     return(
     <div>
         <h1 style={{color:cor}}>{texto}</h1>
-
-        <button onClick={() => {setTexto("Alterado")}}>Mudar</button>
+        <input value={inputText} onChange={(e)=> {setInputText(e.target.value)}} type="text" />
+        <button onClick={mudarTexto}>Mudar</button>
     </div>
     )
 }
